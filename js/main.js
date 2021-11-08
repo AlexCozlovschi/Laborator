2cf18e448caac1c47a2bf3f1f6cc2166b96e7c2d
Alex <alex.cozlvoschi@gmail.com>
2021-11-08 13:31:14 +0200

    Edit for PHP

--- js/main.js
+++ js/main.js
@@ -42,3 +42,31 @@ date = new Date().getFullYear()
 
 addToSelect("bd_date", 1,31)
 addToSelect("bd_year", date - 13, date + 80)
+
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
+
+const reg_form = document.getElementById("formSection")
+
+reg_form.addEventListener('submit', (event)=>{
+    email = reg_form.elements['email'].value
+    //alert(reg_form.elements['email'].value)
+    alert(isEmail(email))
+})
+
+const login = document.getElementById("login_f")
+login.addEventListener('submit',(event)=>{
+     email = login.elements['log_email'].value
+     if (!isEmail(email)){
+         alert("Wrong email input")
+     }
+     else {
+         window.open("http://localhost:63342/Laborator/html/home.html?_ijt=4r7s1slaqsfrvbv7nvttgs5sb0&_ij_reload=RELOAD_ON_SAVE")
+     }
+
+})
+
+
+
